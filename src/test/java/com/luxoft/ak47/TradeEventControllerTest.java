bf997634d11b0b230f5a3a42a4f3b29156daa2a2
agorevalov <79alegor@gmail.com>
2019-04-05 16:21:50 +0200

    Last version + homework

--- src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
+++ src/test/java/com/luxoft/ak47/TradeEventControllerTest.java
@@ -1,5 +1,59 @@
+package com.luxoft.ak47;
+
+import io.restassured.RestAssured;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.text.IsEmptyString.isEmptyOrNullString;
+import static org.hamcrest.xml.HasXPath.hasXPath;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradeEventControllerTest {
+
+
+    @Test
+    void tradeEvent() {
+        RestAssured
+                .given()
+                .when().get("/tradeEvent")
+                .then().statusCode(200);
+    }
+
+    @Test
+    void shouldGet404onNonExistingPage() {
+            when().get("/nonExisting").then().statusCode(404);
+    }
+
+    @Test
+    void shouldReturnTradesWithVersion0() {
+        when().get("/tradeEvent").then().statusCode(200).body("tradeEvent.version", equalTo("0"));
+
+    }
+
+    @Test
+    void shouldCheckTradeId() {
+        String id = "sampleId";
+        when().get("/tradeEvent/{id}", id)
+                .then().statusCode(200)
+                .body("tradeEvent.version", equalTo("0"))
+                .body("tradeEvent.id", equalTo(id))
+                .body(not(hasXPath("tradeEvent/tradeLocation")));
+
+    }
+
+
+
+    @Test
+    void shouldHaveTradeLocationIfFromOBS() {
+        when().get("/tradeEvent/OBS-123").then().body(hasXPath("tradeEvent/tradeLocation"));
+
+    //проверить чтобы был из ОБС и сделать чтобы приходил нормально
+
+    }
+
 
 }
